364eec492e99ff59ceb50549582a37d13586490b
fupan <lifupan@gmail.com>
2018-08-23 17:20:41 +0800

    containerd-shim-kata-v2: update vendor of containerd runtime v2
    update the vendor of containerd runtime v2 to the commit of:
    830363acac529947d794b50c3eec3bc47c59a183: Merge pull request
    
    Signed-off-by: fupan <lifupan@gmail.com>

--- vendor/github.com/containerd/containerd/runtime/v2/shim/shim_windows.go
+++ vendor/github.com/containerd/containerd/runtime/v2/shim/shim_windows.go
@@ -21,9 +21,13 @@ package shim
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"io"
 	"net"
 	"os"
 	"os/exec"
+	"sync"
+	"unsafe"
 
 	winio "github.com/Microsoft/go-winio"
 	"github.com/containerd/containerd/events"
@@ -32,6 +36,7 @@ import (
 	"github.com/containerd/typeurl"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/windows"
 )
 
 // setupSignals creates a new signal handler for all signals
@@ -48,8 +53,43 @@ func subreaper() error {
 	return nil
 }
 
+type fakeSignal struct {
+}
+
+func (fs *fakeSignal) String() string {
+	return ""
+}
+
+func (fs *fakeSignal) Signal() {
+}
+
 func setupDumpStacks(dump chan<- os.Signal) {
-	// TODO: JTERRY75: Make this based on events. signal.Notify(dump, syscall.SIGUSR1)
+	// Windows does not support signals like *nix systems. So instead of
+	// trapping on SIGUSR1 to dump stacks, we wait on a Win32 event to be
+	// signaled. ACL'd to builtin administrators and local system
+	event := "Global\\containerd-shim-runhcs-v1-" + fmt.Sprint(os.Getpid())
+	ev, _ := windows.UTF16PtrFromString(event)
+	sd, err := winio.SddlToSecurityDescriptor("D:P(A;;GA;;;BA)(A;;GA;;;SY)")
+	if err != nil {
+		logrus.Errorf("failed to get security descriptor for debug stackdump event %s: %s", event, err.Error())
+		return
+	}
+	var sa windows.SecurityAttributes
+	sa.Length = uint32(unsafe.Sizeof(sa))
+	sa.InheritHandle = 1
+	sa.SecurityDescriptor = uintptr(unsafe.Pointer(&sd[0]))
+	h, err := windows.CreateEvent(&sa, 0, 0, ev)
+	if h == 0 || err != nil {
+		logrus.Errorf("failed to create debug stackdump event %s: %s", event, err.Error())
+		return
+	}
+	go func() {
+		logrus.Debugf("Stackdump - waiting signal at %s", event)
+		for {
+			windows.WaitForSingleObject(h, windows.INFINITE)
+			dump <- new(fakeSignal)
+		}
+	}()
 }
 
 // serve serves the ttrpc API over a unix socket at the provided path
@@ -77,6 +117,53 @@ func handleSignals(logger *logrus.Entry, signals chan os.Signal) error {
 			}
 		}
 	}
+}
+
+type deferredShimWriteLogger struct {
+	ctx context.Context
+
+	wg sync.WaitGroup
+
+	c      net.Conn
+	conerr error
+}
+
+func (dswl *deferredShimWriteLogger) Write(p []byte) (int, error) {
+	dswl.wg.Wait()
+	if dswl.c == nil {
+		return 0, dswl.conerr
+	}
+	return dswl.c.Write(p)
+}
+
+// openLog on Windows acts as the server of the log pipe. This allows the
+// containerd daemon to independently restart and reconnect to the logs.
+func openLog(ctx context.Context, id string) (io.Writer, error) {
+	ns, err := namespaces.NamespaceRequired(ctx)
+	if err != nil {
+		return nil, err
+	}
+	l, err := winio.ListenPipe(fmt.Sprintf("\\\\.\\pipe\\containerd-shim-%s-%s-log", ns, id), nil)
+	if err != nil {
+		return nil, err
+	}
+	dswl := &deferredShimWriteLogger{
+		ctx: ctx,
+	}
+	// TODO: JTERRY75 - this will not work with restarts. Only the first
+	// connection will work and all +1 connections will return 'use of closed
+	// network connection'. Make this reconnect aware.
+	dswl.wg.Add(1)
+	go func() {
+		c, conerr := l.Accept()
+		if conerr != nil {
+			l.Close()
+			dswl.conerr = conerr
+		}
+		dswl.c = c
+		dswl.wg.Done()
+	}()
+	return dswl, nil
 }
 
 func (l *remoteEventsPublisher) Publish(ctx context.Context, topic string, event events.Event) error {
